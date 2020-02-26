13bd4f121691fab3493005412bb0862322c68f1c
Brian Wolk <brian.wolk@publicissapient.com>
2020-02-26 16:04:49 -0500

    Converted Board to use function instead of class

--- src/Board.js
+++ src/Board.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Board.scss'
 import Time from './Time'
 import FlightTable from './FlightTable'
 
-class Board extends Component {
+function Board() {
 
-	render() {
-		return (
-			<div className="Board">
-				<div className="Board__header">
-					<div className="Board__header__title">FlightBoard</div>
-					<div className="Board__header__time">
-						<Time />
-					</div>
-				</div>
-				<div className="Board__body">
-					<FlightTable />
+	return (
+		<div className="Board">
+			<div className="Board__header">
+				<div className="Board__header__title">FlightBoard</div>
+				<div className="Board__header__time">
+					<Time />
 				</div>
 			</div>
-		)
-	}
+			<div className="Board__body">
+				<FlightTable />
+			</div>
+		</div>
+	)
 }
 
 export default Board
