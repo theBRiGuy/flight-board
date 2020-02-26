39239704a11b16e79a73169c9589d1c9e1446db3
Brian Wolk <brian.wolk@publicissapient.com>
2020-02-26 16:02:39 -0500

    Removed class from App (converting to hooks)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import CssBaseLine from '@material-ui/core/CssBaseline'
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 import Board from './Board'
 
-const theme = createMuiTheme({
-	palette: {
-		type: 'dark'
-	}
-})
+function App() {
 
-class App extends Component {
-	render() {
-		return (
-			<MuiThemeProvider theme={theme}>
-				<div className="App">
-					<CssBaseLine>
-						<main>
-							<Board />
-						</main>
-					</CssBaseLine>
-				</div>
-			</MuiThemeProvider>
-		);
-	}
+	const theme = createMuiTheme({
+		palette: {
+			type: "dark"
+		}
+	})
+
+	return (
+		<MuiThemeProvider theme={theme}>
+			<div className="App">
+				<CssBaseLine>
+					<main>
+						<Board />
+					</main>
+				</CssBaseLine>
+			</div>
+		</MuiThemeProvider>
+	)
 }
 
-export default App;
+export default App
